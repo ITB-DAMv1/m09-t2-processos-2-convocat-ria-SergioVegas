e39c56c16870e6b5f76e131d826abc09330f83df
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-06-09 19:45:56 +0200

    Exercici 4 fet

--- m09-t2-processos-2-convocat-ria-SergioVegas/m09-t2-processos-2-convocat-ria-SergioVegas/Program.cs
+++ m09-t2-processos-2-convocat-ria-SergioVegas/m09-t2-processos-2-convocat-ria-SergioVegas/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace m09_t2_processos_2_convocat_ria_SergioVegas
-{
-    internal class Program
+﻿    using System;
+    using System.Diagnostics;
+    using System.IO;
+
+    namespace Exercici4
     {
-        static void Main(string[] args)
+        class Program
         {
-            Console.WriteLine("Hello, World!");
+            static void Main()
+            {
+                var processes = Process.GetProcesses();
+                int totalProcs = processes.Length, totalThreads = 0;
+                const string output = @"..\..\..\processos.txt";
+
+                using var writer = new StreamWriter(output);
+                foreach (var proc in processes)
+                {
+                    writer.WriteLine($"Procés: {proc.ProcessName} (PID: {proc.Id})");
+                    foreach (ProcessThread th in proc.Threads)
+                    {
+                        writer.WriteLine($"\tFil ID: {th.Id}");
+                        totalThreads++;
+                    }
+                }
+                writer.WriteLine($"Total processos: {totalProcs}");
+                writer.WriteLine($"Total fils: {totalThreads}");
+
+                Console.WriteLine("Resultats desats a processos.txt");
+            }
         }
     }
-}
+

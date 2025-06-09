ce8a8c2279c05e503340eb152314adad9d6ba867
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-06-09 19:37:54 +0200

    Creacio exerici 4

---
+++ m09-t2-processos-2-convocat-ria-SergioVegas/m09-t2-processos-2-convocat-ria-SergioVegas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace m09_t2_processos_2_convocat_ria_SergioVegas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
